70c93289de5cb496fac95eef2d1779f40775411c
Johnson <linghuchongxajh@qq.com>
2025-07-08 14:40:47 +0800

    "阿里云测试也是正常的"

--- backend/slide_agent/slide_agent/config.py
+++ backend/slide_agent/slide_agent/config.py
@@ -8,24 +8,30 @@
 
 ##每个Agent的使用的模型配置
 SPLIT_TOPIC_AGENT_CONFIG = {
-    "provider": "google",
-    "model": "gemini-2.0-flash",
+    # "provider": "google",
+    # "model": "gemini-2.0-flash",
+    "provider": "ali",
+    "model": "qwen-turbo-latest",
 }
 
 TOPIC_RESEARCH_AGENT_CONFIG = {
-    "provider": "google",
-    "model": "gemini-2.0-flash",
+    # "provider": "google",
+    # "model": "gemini-2.0-flash",
     # "provider": "deepseek",
     # "model": "gpt-4.1",
     # "model": "gpt-4.1-nano-2025-04-14",
     # "model": "deepseek-chat",
+    "provider": "ali",
+    "model": "qwen-turbo-latest",
 }
 SUMMARY_AGENT_CONFIG = {
-    "provider": "google",
-    "model": "gemini-2.0-flash",
+    # "provider": "google",
+    # "model": "gemini-2.0-flash",
     # "provider": "openai",
     # "model": "gpt-4.1",
     # "model": "gemini-2.0-flash",
-    # "provider": "local_ali",
+    # "provider": "local_ali",  # 如果使用local_ali需要使用tools/LLM_cache.py用于中间缓存
     # "model": "qwen-turbo-latest",
+    "provider": "ali",
+    "model": "qwen-turbo-latest",
 }
